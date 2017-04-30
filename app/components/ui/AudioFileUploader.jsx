1cf767414535b4b49db4011274758ec25ad168bf
uraway <masato.uraway@gmail.com>
2017-04-30 13:52:41 +0800

    stream

--- app/components/ui/AudioFileUploader.jsx
+++ app/components/ui/AudioFileUploader.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ipcRenderer } from 'electron';
 
 const styles = {
   input: {
@@ -15,23 +16,85 @@ const styles = {
 
 export default class AudioFileUploader extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      model: 'ja-JP_BroadbandModel',
+      formattedMessages: [],
+      audioSource: null,
+      speakerLabels: false,
+    };
+  }
+
   handleAudioFileUpload = (e) => {
     const file = e.target.files[0];
-    const reader = new FileReader();
+    const path = file.path;
 
     if (file) {
-      reader.readAsDataURL(file);
+      ipcRenderer.send('speech-to-text', path);
       e.target.value = null;
     } else {
       console.error('Upload Failed');
       return false;
     }
 
-    reader.onload = () => {
-      console.log(reader);
-    };
-
     return true;
+  }
+
+  handleStream = (stream) => {
+    this.stream = stream;
+    this.captureSettings();
+
+    // grab the formatted messages and also handle errors and such
+    stream.on('data', this.handleFormattedMessage).on('end', this.handleTranscriptEnd).on('error', this.handleError);
+
+    // when errors occur, the end event may not propagate through the helper streams.
+    // However, the recognizeStream should always fire a end and close events
+    stream.recognizeStream.on('end', () => {
+      if (this.state.error) {
+        console.log('end');
+      }
+    });
+
+  // grab raw messages from the debugging events for display on the JSON tab
+    stream.recognizeStream
+    .on('message', (frame, json) => this.handleRawdMessage({ sent: false, frame, json }))
+    .on('send-json', json => this.handleRawdMessage({ sent: true, json }))
+    .once('send-data', () => this.handleRawdMessage({
+      sent: true, binary: true, data: true // discard the binary data to avoid waisting memory
+    }))
+    .on('close', (code, message) => this.handleRawdMessage({ close: true, code, message }));
+
+  // ['open','close','finish','end','error', 'pipe'].forEach(e => {
+  //     stream.recognizeStream.on(e, console.log.bind(console, 'rs event: ', e));
+  //     stream.on(e, console.log.bind(console, 'stream event: ', e));
+  // });
+  }
+
+  handleRawdMessage = (msg) => {
+    this.setState({ rawMessages: this.state.rawMessages.concat(msg) });
+    console.log(msg);
+  }
+
+  getRecognizeOptions(extra) {
+    return Object.assign({
+      smart_formatting: true, // formats phone numbers, currency, etc. (server-side)
+      format: true, // adds capitals, periods, and a few other things (client-side)
+      model: this.state.model,
+      objectMode: true,
+      interim_results: true,
+      continuous: true,
+      timestamps: true, // set timestamps for each word - automatically turned on by speaker_labels
+    }, extra);
+  }
+
+  captureSettings = () => {
+    this.setState({
+      settingsAtStreamStart: {
+        model: this.state.model,
+        speakerLabels: this.state.speakerLabels
+      }
+    });
   }
 
   render() {
--- app/main.development.js
+++ app/main.development.js
@@ -1,6 +1,10 @@
-/* eslint global-require: 1, flowtype-errors/show-errors: 0 */
+/* eslint global-require: 0, flowtype-errors/show-errors: 0 */
 // @flow
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
+import { download } from 'electron-dl';
+import SpeechToTextV1 from 'watson-developer-cloud/speech-to-text/v1';
+import fs from 'fs';
+
 import MenuBuilder from './menu';
 
 let mainWindow = null;
@@ -69,4 +73,54 @@ app.on('ready', async () => {
 
   const menuBuilder = new MenuBuilder(mainWindow);
   menuBuilder.buildMenu();
+});
+
+ipcMain.on('speech-to-text', (e, path) => {
+  const speechToText = new SpeechToTextV1({
+    username: 'f19ce38c-7c76-4a31-a215-62f2873c77d6',
+    password: 'pETNBPoPIofK'
+  });
+
+  const params = {
+    model: 'ja-JP_BroadbandModel',
+    content_type: 'wav',
+    continuous: true,
+    interim_results: true,
+    max_alternatives: 3,
+    word_confidence: false,
+    timestamps: false,
+    keywords: ['colorado', 'tornado', 'tornadoes'],
+    keywords_threshold: 0.5
+  };
+
+  // Create the stream.
+  const recognizeStream = speechToText.createRecognizeStream(params);
+
+  // Pipe in the audio.
+  fs.createReadStream(path).pipe(recognizeStream);
+
+  const tempFilePath = `${__dirname}/tmp/transcription.txt`;
+  // Pipe out the transcription to a file.
+  recognizeStream.pipe(fs.createWriteStream(tempFilePath));
+  // Get strings instead of buffers from 'data' events.
+  recognizeStream.setEncoding('utf8');
+
+  // Displays events on the console.
+  function onEvent(name, event) {
+    if (name === 'Close:') {
+      download(BrowserWindow.getFocusedWindow(), tempFilePath, { saveAs: true });
+    }
+    console.log(name, JSON.stringify(event, null, 2));
+  }
+
+  // Listen for events.
+  recognizeStream.on('results', (event) => { onEvent('Results:', event); });
+  recognizeStream.on('data', (event) => { onEvent('Data:', event); });
+  recognizeStream.on('error', (event) => { onEvent('Error:', event); });
+  recognizeStream.on('close', (event) => { onEvent('Close:', event); });
+  recognizeStream.on('speaker_labels', (event) => { onEvent('Speaker_Labels:', event); });
+});
+
+ipcMain.on('download-file', (event, file) => {
+  download(BrowserWindow.getFocusedWindow(), file, { saveAs: true });
 });
