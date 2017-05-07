b42a00ced781f1c8732ab34d6831e6765a1a3727
uraway <masato.uraway@gmail.com>
2017-05-07 22:57:57 +0800

    change colors

--- app/containers/App.js
+++ app/containers/App.js
@@ -2,8 +2,22 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import DefaultBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import AppCanvas from 'material-ui/internal/AppCanvas';
+import { purple900 } from 'material-ui/styles/colors';
+
+const muiTheme = getMuiTheme({
+  tabs: {
+    backgroundColor: purple900,
+  },
+  raisedButton: {
+    textColor: '#ffffff',
+    color: purple900,
+  },
+  textField: {
+    floatingLabelColor: purple900,
+    focusColor: purple900
+  }
+});
 
 export default class App extends Component {
   static propTypes = {
@@ -16,7 +30,7 @@ export default class App extends Component {
 
   getChildContext() {
     return {
-      muiTheme: getMuiTheme(DefaultBaseTheme),
+      muiTheme: getMuiTheme(muiTheme),
     };
   }
 
